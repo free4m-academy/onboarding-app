3b64d796f8f6a916538d37215f96e99398bac24c
Zhana Liner <zhana@beviabl.com>
2026-03-12 14:18:40 -0700

    run Prettier on everything

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,3 +1,3 @@
 export const CYAN = '#00d4ff'; // Cyan
-export const PINK = '#bb33ff'; // Pink/Purple
+export const PINK = "#bb33ff"; // Pink/Purple
 export const NAVY = '#050d2e'; // Dark navy blue
